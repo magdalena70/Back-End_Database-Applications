bbd7df97162d44812be3a8307f73e5bf87b395f7
Magdalena Petrova <magik70@abv.bg>
2015-08-01 17:00:07 +0300

    JSON_Demo

---
+++ JSON_Demo/JavaScriptSerializerDemo/JavaScriptSerializerDemo.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json; //in console - Install-Package Newtonsoft.Json
+using Newtonsoft.Json.Linq; //Contained in System.Web.Extensions assembly
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace JavaScriptSerializerDemo
+{
+    class JavaScriptSerializerDemo
+    {
+        static void Main()
+        {
+            var digits = new Dictionary<string, int>
+            {
+                { "one", 1 },
+                { "two", 2 },
+                { "three", 3 },
+                { "four", 4 },
+                { "five", 5 },
+                { "six", 6 },
+                { "seven", 7 },
+                { "eight", 8 },
+                { "nine", 9 },
+            };
+
+            var serializer = new JavaScriptSerializer();
+            var digitsInJson = serializer.Serialize(digits);
+            Console.WriteLine("Serialize:\n{0}", digitsInJson);
+
+            var digitsDeserialized = serializer.Deserialize<Dictionary<string, int>>(digitsInJson);
+            Console.WriteLine("Deserialize:");
+            foreach (var pair in digitsDeserialized)
+            {
+                Console.WriteLine("{0} -> {1}", pair.Key, pair.Value);
+            }
+
+            //-----JSON.NET----------//
+            var jsonFormatting = JsonConvert.SerializeObject(digits, Formatting.Indented);
+            Console.WriteLine("formatting: {0}\n", jsonFormatting);
+
+            var json =
+                @"{
+                  'firstName': 'Vladimir',
+                  'lastName': 'Georgiev',
+                  'jobTitle': 'Technical Trainer'
+                }";
+
+            var template = new
+            {
+                FirstName = string.Empty,
+                Lastname = string.Empty,
+                JobTitle = string.Empty
+            };
+
+            var person = JsonConvert.DeserializeAnonymousType(json, template);
+            Console.WriteLine(person);
+
+            //-----LINQ to JSON---------//
+            var jsonObj = JObject.Parse(json);
+            Console.WriteLine("\nFirst name: {0}", jsonObj["firstName"]);
+        }
+    }
+}
---
+++ JSON_Demo/LINQtoJSON/LINQtoJSON.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json.Linq; ////in console - Install-Package Newtonsoft.Json
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQtoJSON
+{
+    class LINQtoJSON
+    {
+        static void Main()
+        {
+            var json = @"
+            {
+              name: 'Sofia',
+              places: [{
+               'name': 'Pri Ivan',
+               'categories': [
+                 {
+                   'id': 1,
+                   'name': 'food'
+                 },
+                 {
+                   'id': 2,
+                   'name': 'drinks'
+                 }
+               ],
+               'location': {
+                 'latitude': 42.69751,
+                 'longitude': 23.32415
+               }
+             },{
+               'name': 'Golyamata Parjola',
+               'categories': [
+                 {
+                   'id': 1,
+                   'name': 'food'
+                 },
+                 {
+                   'id': 3,
+                   'name': 'big meat'
+                 }
+               ],
+               'location': {
+                 'latitude': 43.69751,
+                 'longitude': 23.32415
+               }
+             },{
+               'name': 'Dalbokoto dano',
+               'categories': [
+                 {
+                   'id': 4,
+                   'name': 'beer'
+                 },
+                 {
+                   'id': 2,
+                   'name': 'drinks'
+                 }
+               ],
+               'location': {
+                 'latitude': 42.1,
+                 'longitude': 23.2
+               }
+             }]
+            }";
+
+            var jsonObj = JObject.Parse(json);
+
+            Console.WriteLine("Places in {0}:", jsonObj["name"]);
+            var index = 1;
+
+            var result = jsonObj["places"]
+                .Select(pl =>
+                    string.Format("{0}) {1} ({2})",
+                    index++, 
+                    pl["name"],
+                    string.Join(", ", pl["categories"].Select(cat => cat["name"]))));
+            foreach (var pair in result)
+            {
+                Console.WriteLine("{0}", pair);
+            }  
+        }
+    }
+}
